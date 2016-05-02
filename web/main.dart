23ae11305ec5a29d215b6fc958e19600afad489d
yzcheng <yongzong.chen@gmail.com>
2016-05-03 07:37:06 +0800

    first commit

---
+++ web/main.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/platform/browser.dart';
+
+import 'package:dart_example/app_component.dart';
+
+import 'package:polymer_elements/paper_button.dart';
+import 'package:polymer_elements/paper_toolbar.dart';
+import 'package:polymer_elements/paper_icon_button.dart';
+import 'package:polymer_elements/iron_icon.dart';
+import 'package:polymer_elements/iron_icons.dart';
+
+main() {
+  bootstrap(AppComponent);
+}
